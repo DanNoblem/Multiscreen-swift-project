1065ce2ca8ab09856a231d34e294868ea8b9976a
DanNoble <danielwai999@gmail.com>
2023-02-14 16:38:41 -0500

    Multi-screen App test

---
+++ Multiscreen swift project/Homepage.swift
@@ -0,0 +1,31 @@
+//
+//  File.swift
+//  Multiscreen swift project
+//
+//  Created by Daniel Wai on 2/14/23.
+//
+
+import SwiftUI
+
+struct homepage: View {
+    var body: some View {
+        NavigationView {
+            List {
+                NavigationLink {
+                    Welcome()
+                } label: {
+                    Text("Welcome")
+                }
+            }
+        }.navigationTitle("Homepage")
+    }
+}
+
+struct Homepage_Preview: PreviewProvider {
+  static var previews: some View {
+    homepage()
+  }
+}
+
+
+
